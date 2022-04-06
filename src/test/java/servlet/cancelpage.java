580565d4c1535fc18a26a380f572a99ea5d6b367
Amarpalli <102972532+Amarpalli@users.noreply.github.com>
2022-04-06 15:56:05 +0530

    Update cancelpage.java
    updated private IP

--- src/test/java/servlet/cancelpage.java
+++ src/test/java/servlet/cancelpage.java
@@ -17,7 +17,7 @@ public class cancelpage extends HttpServlet {
 				try
 				{
 					Class.forName("org.postgresql.Driver");
-					String jdbcUrl="jdbc:postgresql://172.31.17.59:5432/postgres";
+					String jdbcUrl="jdbc:postgresql://172.31.30.99:5432/postgres";
 					String username="postgres";
 					String password="password";
 					
